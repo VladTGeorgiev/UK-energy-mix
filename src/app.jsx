80e95d311f4a8e601a2c7f6caa7d824180dd73a5
VladTGeorgiev <vlad.georgiev@outlook.com>
2019-10-23 21:22:39 +0100

    Mobile responsivness added

--- src/app.jsx
+++ src/app.jsx
@@ -10,17 +10,28 @@ class App extends Component {
 		this.state = {
 			data: {}
 		};
-	}
+	};
 
 	componentDidMount () {
+		window.addEventListener('resize', this.handleWindowSizeChange);
 		API.fetchData().then((data) => {
 			this.setState({
 				data: data
 			});
 		});
-	}
+	};
+
+	componentWillUnmount () {
+		window.removeEventListener('resize', this.handleWindowSizeChange);
+	};
+
+	handleWindowSizeChange = () => {
+		this.setState({ width: window.innerWidth });
+	};
 
 	render () {
+		const output = this.state.data.data === undefined ? null : this.state.data.data;
+		const dashboardLayout = this.state.width < 1000 ? 1 : 2;
 
 		if (this.state.data.error) {
 			return (
@@ -38,7 +49,48 @@ class App extends Component {
 		} else {
 			return (
 				<Container>
-
+					<Divider hidden />
+					<Header as="h1">UK Energy Mix</Header>
+					<Divider />
+					{output === null ? (
+						<Container>
+							<Divider hidden />
+							<Dimmer active inverted>
+								<Loader size="large">Loading</Loader>
+							</Dimmer>
+						</Container>
+					) : (
+						<Container>
+							<Container className="dashboard">
+								<Header as="h2">Fuel types overview</Header>
+								<Grid divided="vertically">
+									<Grid.Row columns={dashboardLayout}>
+										<Grid.Column />
+										<Grid.Column>
+											<Divider hidden />
+											<Divider hidden />
+											<Label className="dashboard-date">
+												<Divider hidden fitted />
+												<Icon name="time" />
+												Values for the period
+												<Divider hidden fitted />
+												from :
+												<Divider hidden fitted />
+												<Label color="yellow">{output.from}</Label>
+												<Divider hidden fitted />
+												to:
+												<Divider hidden fitted />
+												<Label color="yellow">{output.from}</Label>
+											</Label>
+										</Grid.Column>
+									</Grid.Row>
+								</Grid>
+								<Divider />
+							</Container>
+							<Header as="h2">Fuel types sorted by percentage energy generated</Header>
+							<Divider hidden />
+						</Container>
+					)}
 				</Container>
 			);
 		}
